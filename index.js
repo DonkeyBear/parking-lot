899d6d8f1f8ad211ccf8f3f022c80d13085de938
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-09 16:30:30 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+app.set('port', (process.env.PORT || 5000));
+
+app.get('/', function(request, response) {
+	var result = 'App is running'
+	response.send(result);
+}).listen(app.get('port'), function() {
+	console.log('App is running, server is listening on port ', app.get('port'));
+});
 
 app.listen(port, () => {
-  console.log('OK')
+	console.log('OK')
 })
