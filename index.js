65452e098fc157847a989b815ed9f5e89ef6bf74
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-16 15:41:15 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 const express = require('express')
 const app = express()
-const { Client } = require('pg')
-const client = new Client()
-await client.connect()
+const fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000));
 	
-app.get('/get', function(req, res) {
-	const q = await client.query('SELECT $1::text as message', ['Hello world!'])
-	console.log(q.rows[0].message) // Hello world!
-	await client.end()
+app.get('/get', function(req, res) {	
+	fs.readFile('/sensor.json', 'utf8' , (err, data) => {
+		if (err) { console.error(err) }
+		res.json(JSON.parse(data))
+	})
 }).listen(app.get('port'), function() {
 	console.log('App is running, server is listening on port ', app.get('port'));
 });
