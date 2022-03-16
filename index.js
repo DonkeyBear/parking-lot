3382a9a38b8749f8e9713a3998227698c7fc4ffc
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-16 15:10:36 +0800

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,22 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+import { join, dirname } from 'path'
+import { Low, JSONFile } from 'lowdb'
+import { fileURLToPath } from 'url'
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const file = join(__dirname, 'db.json')
+const adapter = new JSONFile(file)
+const db = new Low(adapter)
+await db.read()
+db.data ||= { sensor: [] }
+await db.write()
+
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/', function(request, response) {
-	var result = 'App is running'
-	response.send(result);
+app.get('/get', function(request, response) {
+	res.json(db.data)
 }).listen(app.get('port'), function() {
 	console.log('App is running, server is listening on port ', app.get('port'));
 });
