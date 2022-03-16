26e9b557d7df4cba9bfe36100f8c023690b1a60c
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-16 15:49:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,10 +4,16 @@ const fs = require('fs')
 
 app.set('port', (process.env.PORT || 5000));
 	
-app.get('/get', function(req, res) {	
+app.get('/', function(req, res) {
 	fs.readFile('sensor.json', 'utf8' , (err, data) => {
-		if (err) { console.error(err) }
+		if (err) { console.error(err) ; return ;}
 		res.json(JSON.parse(data))
+	})
+}).post('/', function(req, res) {
+	if(req.param.sensor.length != 10) return;
+	fs.writeFile('sensor.json', req.param.sensor, err => {
+		if (err) { console.error(err) ; return ;}
+		res.ok();
 	})
 }).listen(app.get('port'), function() {
 	console.log('App is running, server is listening on port ', app.get('port'));
