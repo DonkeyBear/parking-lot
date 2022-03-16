96ec272609a441ff04119336102b624c51d2a183
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-16 15:26:16 +0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.set('port', (process.env.PORT || 5000));
 
-app.get('/get', function(request, response) {
+app.get('/get', function(req, res) {
 	res.json([0,0,0,0,0,1,1,1,1,1])
 }).listen(app.get('port'), function() {
 	console.log('App is running, server is listening on port ', app.get('port'));
