05b566f7a0679c317cc077eb4f871c47e304abb6
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-16 15:52:59 +0800

    Update index.js

--- index.js
+++ index.js
@@ -10,10 +10,16 @@ app.get('/', function(req, res) {
 		res.json(JSON.parse(data))
 	})
 }).post('/', function(req, res) {
-	if(req.param.sensor.length != 10) return;
+	if(req.param.sensor.length != 10) {
+		res.status(400).json({"error":"data"})
+		return;
+	}
 	fs.writeFile('sensor.json', req.param.sensor, err => {
 		if (err) { console.error(err) ; return ;}
-		res.ok();
+		fs.readFile('sensor.json', 'utf8' , (err2, data) => {
+			if (err2) { console.error(err2) ; return ;}
+			res.json(JSON.parse(data))
+		})
 	})
 }).listen(app.get('port'), function() {
 	console.log('App is running, server is listening on port ', app.get('port'));
