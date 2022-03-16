bec772b9dca41adadbddbe1e16132ed28d981b6f
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-16 15:36:12 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const express = require('express')
 const app = express()
+const { Client } = require('pg')
+const client = new Client()
+await client.connect()
 
 app.set('port', (process.env.PORT || 5000));
-
+	
 app.get('/get', function(req, res) {
-	res.json([0,0,0,0,0,1,1,1,1,1])
+	const q = await client.query('SELECT $1::text as message', ['Hello world!'])
+	console.log(q.rows[0].message) // Hello world!
+	await client.end()
 }).listen(app.get('port'), function() {
 	console.log('App is running, server is listening on port ', app.get('port'));
 });
