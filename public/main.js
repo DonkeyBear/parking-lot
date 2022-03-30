d2e6f8c5f0f1e05b3c5e2773b071ddacff5d7674
DonkeyBear <35475647+DonkeyBear@users.noreply.github.com>
2022-03-30 16:52:58 +0800

    3/30

--- public/main.js
+++ public/main.js
@@ -40,6 +40,44 @@ document.querySelector("#floor-select").onchange = () => {
   getEmptyParkingLots();
 }
 
+let lastDataString;
+let parkingLots = ["101", "102", "103", "104", "105", "106", "107", "108", "109", "110", "201", "202", "203", "204", "205", "206", "207", "208", "209", "210"];
+
+function recentInfo(dataString) {
+  if (typeof lastDataString === "undefined") {
+    lastDataString = dataString;
+  } else if (lastDataString != dataString) {
+    // let logObject = {};
+    let infoString;
+    for (let i = 0; i < dataString.length; i++) {
+      if (lastDataString.substr(i, 1) != dataString.substr(i, 1)) {
+        if (lastDataString.substr(i, 1) == 1) {
+          // a parking lot just vacated.
+          // logObject[i] = "vacated";
+          infoString = parkingLots[i] + "號車位現在空出來了！"
+          break;
+        } else {
+          // a parking lot has just been parked.
+          // logObject[i] = "parked";
+          infoString = parkingLots[i] + "號車位已經被停走了。"
+          break;
+        }
+      }
+    }
+    document.querySelector(".toast-body").innerText = infoString;
+    redSpotShow();
+  }
+}
+
+function redSpotShow() {
+  // 音楽：魔王魂
+  // 原名：魔王魂 効果音 システム46.ogg
+  let se = new Audio('./hint_sound.ogg');
+  se.play();
+  document.querySelector("#red-spot").style.display = "unset";
+  $("#red-spot").fadeOut(speed = 1500);
+}
+
 if (location.protocol !== 'https:') location.replace(`https:${location.href.substring(location.protocol.length)}`);
 $(() => {
   function getSensor() {
@@ -47,6 +85,7 @@ $(() => {
       url: 'https://iot-parking-lot.herokuapp.com/api',
       success: (res) => {
         colorParkingLots(res);
+        recentInfo(res);
       }
     });
     getEmptyParkingLots();
